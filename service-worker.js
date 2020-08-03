e6923eafdda66b398e5bfed7bcbe8968b364ade6
Erikken-Hione <68046024+Erikken-Hione@users.noreply.github.com>
2020-08-03 15:28:27 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Erikken-Hione/dp_test/precache-manifest.bc8af77ce12318be4af43c9f12453d51.js"
+  "/dp_test/precache-manifest.e658c837d4a36333f3fd4121b314b860.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Erikken-Hione/dp_test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dp_test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
